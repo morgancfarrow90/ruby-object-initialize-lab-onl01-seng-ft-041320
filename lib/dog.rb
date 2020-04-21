f8dcc03a6aaa8a316c9f5c2a35dfebe89834e8b6
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-21 16:48:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,3 @@
-class Person
-  def initialize (persons_name)
-    @name = persons_name
-  end
-
-  def name=(persons_name)
-    @name = persons_name
-  end
-
-  def name
-    @name
-  end
-  
-end
-
 class Dog
   def initialize (dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
